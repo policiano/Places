046c12ae18a14751fe63e23d120be822cdd6ee8e
Willian Policiano <willian.policiano@ifood.com.br>
2020-07-14 10:50:32 -0300

    Create Repository module

---
+++ Repository/Resources/Repository.h
@@ -0,0 +1,19 @@
+//
+//  Repository.h
+//  Repository
+//
+//  Created by Willian Fagner De Souza Policiano on 14/07/20.
+//  Copyright © 2020 Willian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Repository.
+FOUNDATION_EXPORT double RepositoryVersionNumber;
+
+//! Project version string for Repository.
+FOUNDATION_EXPORT const unsigned char RepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Repository/PublicHeader.h>
+
+
---
+++ Repository/Tests/RespoTests.swift
@@ -0,0 +1,10 @@
+import Foundation
+import XCTest
+
+@testable import Repository
+
+final class RepoTests: XCTestCase {
+    func test_() {
+        
+    }
+}
